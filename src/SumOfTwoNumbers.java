9b1e993b75bde4618ae0ccb9bc3ffe4d3819e73f
Neha Malik <neha.malik@caroobi.com>
2019-06-08 17:35:31 +0530

    on my second branch

---
+++ src/SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+
+public class SumOfTwoNumbers {
+	public static void main (String [] args) {
+		
+	
+	int a=9,b=1;
+	int sum=0;
+	
+    sum=a+b;
+    System.out.println("sum is :  " + sum);
+	}
+}
